174a7da5300514ac71c14511d6e58740747bb210
333 <333u@protonmail.com>
2020-06-08 14:43:20 +0200

    remove old code

--- draw.h
+++ draw.h
@@ -3,16 +3,4 @@ struct text_box {
 	int color;
 };
 
-
 void drawf(uint8_t r, uint8_t c, const char *format, ...);
-
-
-void draw_box_str(char, int, short, short, const char *);
-
-void draw_box_num(char, int, short, short, int);
-//void draw_column_num(char, int, short, short, int *, size_t);
-void draw_table_num(char, int, short, short, short gap,
-               size_t n_rows, size_t n_cols, int *table);
-
-void draw_row_numbering(char, short, short, short, short);
-void draw_column_numbering(char, short, short, short, short);
